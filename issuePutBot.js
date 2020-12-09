ccb756011ad44cf9f5c6108159f1438ee9477a09
Ubuntu <ubuntu@ip-172-31-13-132.ap-northeast-2.compute.internal>
2020-12-09 08:28:43 +0000

    huh?

--- issuePutBot.js
+++ issuePutBot.js
@@ -1,2 +1,7 @@
 /* Bot: IF new article on SLACK, THEN makes a new issue on GITLAB. */
 const { Gitlab } = require('@gitbeaker/node');
+const api = new Gitlab({
+    host: 'https://git.promedius.dev',
+    token: 'VX32ZQCRyxL16D4H1f66',
+  });
+  
