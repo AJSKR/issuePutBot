741fe2bbd1f4e30b220eb0b85b2b3565c8bd966a
Ubuntu <ubuntu@ip-172-31-13-132.ap-northeast-2.compute.internal>
2020-12-09 08:41:24 +0000

    short impl. works well now.

--- issuePutBot.js
+++ issuePutBot.js
@@ -5,3 +5,24 @@ const api = new Gitlab({
   token: 'VX32ZQCRyxL16D4H1f66',
 });
 
+require('http').createServer((req,res)=>{
+  let body = [];
+  req.on('data', chunk => {
+    body.push(chunk);
+  }).on('end', async ()=> {
+    let content;
+    try {
+      content = JSON.parse(Buffer.concat(body).toString());
+    } catch(e) {
+      console.error(e);
+    }
+    console.log(req.method, req.url, content);
+    const { title, description } = content;
+    if (title && description) {
+      await api.Issues.create(130, {
+        title, description
+      });
+    }
+    res.end("ok");
+  });
+}).listen(8080);
